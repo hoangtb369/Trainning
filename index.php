e2fcf60fa3c41a4cac3d3f0a626412fc5fe6588f
hoangtb369 <46711172+hoangtb369@users.noreply.github.com>
2022-11-21 10:41:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -21,21 +21,17 @@ h1,h2,h3,h4,h5,h6{font-size:100%;font-weight:normal}
 q:before,q:after{content:''}
 abbr,acronym{border:0}
 #unknown h2{position:absolute;top:50%;line-height:100px;height:100px;margin-top:-50px;font-size:100px;width:100%;text-align:center;color:transparent;-webkit-animation:blurFadeInOut 3s ease-in backwards;-moz-animation:blurFadeInOut 3s ease-in backwards;-ms-animation:blurFadeInOut 3s ease-in backwards;animation:blurFadeInOut 3s ease-in backwards}
-#unknown h2.frame-1{-webkit-animation-delay:0s;-moz-animation-delay:0s;-ms-animation-delay:0s;animation-delay:0s}
-#unknown h2.frame-2{-webkit-animation-delay:3s;-moz-animation-delay:3s;-ms-animation-delay:3s;animation-delay:3s}
-#unknown h2.frame-3{-webkit-animation-delay:6s;-moz-animation-delay:6s;-ms-animation-delay:6s;animation-delay:6s}
-#unknown h2.frame-4{font-size:200px;-webkit-animation-delay:9s;-moz-animation-delay:9s;-ms-animation-delay:9s;animation-delay:9s}
 #unknown h2.frame-5{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none;color:transparent;text-shadow:0px 0px 1px #fff:margin-top:30px;}
-#unknown h2.frame-5 span{-webkit-animation:blurFadeIn 3s ease-in 12s backwards;-moz-animation:blurFadeIn 1s ease-in 12s backwards;-ms-animation:blurFadeIn 3s ease-in 12s backwards;animation:blurFadeIn 3s ease-in 12s backwards;color:transparent;text-shadow:0px 0px 1px #fff}
-#unknown h2.frame-5 span:nth-child(2){-webkit-animation-delay:13s;-moz-animation-delay:13s;-ms-animation-delay:13s;animation-delay:13s}
-#unknown h2.frame-5 span:nth-child(3){-webkit-animation-delay:14s;-moz-animation-delay:14s;-ms-animation-delay:14s;animation-delay:14s}
-#unknown h2.frame-5 span:nth-child(4){-webkit-animation-delay:15s;-moz-animation-delay:15s;-ms-animation-delay:15s;animation-delay:15s}
-#unknown h2.frame-5 span:nth-child(5){-webkit-animation-delay:16s;-moz-animation-delay:16s;-ms-animation-delay:16s;animation-delay:16s}
+#unknown h2.frame-5 span{-webkit-animation:blurFadeIn 3s ease-in 0s backwards;-moz-animation:blurFadeIn 1s ease-in 0s backwards;-ms-animation:blurFadeIn 3s ease-in 0s backwards;animation:blurFadeIn 3s ease-in 0s backwards;color:transparent;text-shadow:0px 0px 1px #fff}
+#unknown h2.frame-5 span:nth-child(2){-webkit-animation-delay:1s;-moz-animation-delay:1s;-ms-animation-delay:1s;animation-delay:1s}
+#unknown h2.frame-5 span:nth-child(3){-webkit-animation-delay:2s;-moz-animation-delay:2s;-ms-animation-delay:2s;animation-delay:2s}
+#unknown h2.frame-5 span:nth-child(4){-webkit-animation-delay:3s;-moz-animation-delay:3s;-ms-animation-delay:3s;animation-delay:3s}
+#unknown h2.frame-5 span:nth-child(5){-webkit-animation-delay:4s;-moz-animation-delay:4s;-ms-animation-delay:4s;animation-delay:4s}
 #unknown h2.frame-6 span{-webkit-animation:blurFadeIn 3s ease-in 12s backwards;-moz-animation:blurFadeIn 1s ease-in 19s backwards;-ms-animation:blurFadeIn 3s ease-in 19s backwards;animation:blurFadeIn 3s ease-in 19s backwards;color:#FF0B0A;text-shadow:0px 0px 2px #fff;margin-top:500px;line-height:50px;}
-#unknown h2.frame-6 span:nth-child(2){-webkit-animation-delay:20s;-moz-animation-delay:20s;-ms-animation-delay:20s;animation-delay:20s;font-size:20px;color:#FFF;}
-#unknown h2.frame-6 span:nth-child(3){-webkit-animation-delay:20s;-moz-animation-delay:20s;-ms-animation-delay:20s;animation-delay:20s;}
-#unknown h2.frame-6 span:nth-child(4){-webkit-animation-delay:20s;-moz-animation-delay:20s;-ms-animation-delay:20s;animation-delay:20s;}
-#unknown h2.frame-6 span:nth-child(5){-webkit-animation-delay:20s;-moz-animation-delay:20s;-ms-animation-delay:20s;animation-delay:20s;font-size:20px;color:#FFF;}
+#unknown h2.frame-6 span:nth-child(2){-webkit-animation-delay:4s;-moz-animation-delay:4s;-ms-animation-delay:4s;animation-delay:4s;font-size:20px;color:#FFF;}
+#unknown h2.frame-6 span:nth-child(3){-webkit-animation-delay:4s;-moz-animation-delay:4s;-ms-animation-delay:4s;animation-delay:4s;}
+#unknown h2.frame-6 span:nth-child(4){-webkit-animation-delay:4s;-moz-animation-delay:4s;-ms-animation-delay:4s;animation-delay:4s;}
+#unknown h2.frame-6 span:nth-child(5){-webkit-animation-delay:4s;-moz-animation-delay:4s;-ms-animation-delay:4s;animation-delay:4s;font-size:20px;color:#FFF;}
 
 
 .unknown1{position:absolute;width:282px;height:273px;left:50%;top:50%;margin:0;background:none;-webkit-animation:fadeInBack 3.6s linear 14s backwards;-moz-animation:fadeInBack 3.6s linear 14s backwards;-ms-animation:fadeInBack 3.6s linear 14s backwards;animation:fadeInBack 3.6s linear 14s backwards;-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=30)";filter:alpha(opacity=30);opacity:0.3;-webkit-transform:scale(2);-moz-transform:scale(2);-o-transform:scale(2);-ms-transform:scale(2);transform:scale(2)}
